dc62aa25b63b3e9e19b5b2cc1923eacbae8b4138
Ryan.Z <a760504627@gmail.com>
2015-04-15 19:48:15 +1000

    V1.6
    Draft Function 2, debug undergoing

--- F2_V_1_5/Triwave_Generator.v
+++ F2_V_1_5/Triwave_Generator.v
@@ -1,15 +1,20 @@
 module Triwave_Generator(input wire sysclk,
 input wire Enable_SW_2,
-output wire Pulse);
+input wire [2:0] Clip_Factor,
+output wire [6:0] Duty_Output);
+
 reg [5:0] count = 0;
 reg [5:0] DC_Index = 0;
 reg [6:0] Duty_Cycle = 0;
-assign Pulse = (count < Duty_Cycle & Enable_SW_2);
+
+assign Duty_Output = (Duty_Cycle/Clip_Factor) * Enable_SW_2;
+
 always @(posedge sysclk)begin
 	count<=count+1'b1;	
 	if (&count==1)
 		DC_Index<=DC_Index+1'b1;
 	end
+	
 always @ (DC_Index) begin
 	if (6'd0<DC_Index & DC_Index<6'd32)begin
 		Duty_Cycle <= Duty_Cycle + 2'd2;
