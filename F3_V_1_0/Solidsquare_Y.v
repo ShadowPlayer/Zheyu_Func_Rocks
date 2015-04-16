cc48440c304efac4b1b5a608056c7d8369f99926
sccalex <ruixinhan@Ruixins-MacBook-Pro.local>
2015-04-16 15:23:05 +1000

    F3 V1.0.2
    Solid Square Draft

--- F3_V_1_0/Solidsquare_Y.v
+++ F3_V_1_0/Solidsquare_Y.v
@@ -1,9 +1,10 @@
-module Solidsquare_Y(input wire sysclk,
+module Square_Y(input wire sysclk,
 input wire Enable_SW_3,
 output wire Pulse);
 
 reg [5:0] count = 0;
 reg [5:0] DC_Index = 0;
+reg [5:0] Amp_Index = 0;
 reg [6:0] Duty_Cycle = 0;
 
 assign Pulse = (Count < Duty_Cycle) * Enable_SW_3;
@@ -11,11 +12,14 @@ assign Pulse = (Count < Duty_Cycle) * Enable_SW_3;
 always @(posedge sysclk)begin
 	count<=count+1'b1;
 	if (&count==1)
-		DC_Index<=DC_Index+1'b1;	
+		DC_Index<=DC_Index+1'b1;
+	if (&DC_Index==1)
+		Amp_Index<=Amp+1'b1;	
 	end
 always @ (DC_Index)begin
-	if (DC_Index<6'd32)begin
-		Duty_Cycle <= 7'd64; // 64 making the Pulse determination block can achieve 100% i.e. == 63 
+	if (6d'15 < DC_Index & DC_Index <6'd48)begin
+		Duty_Cycle <= Amp_Index; /* 50% duty cycle Square wave with  
+		amplitude during each period varying from 0% to approximately 100%*/
 	end else begin	
 		Duty_Cycle <= 6'b0;
 	end
